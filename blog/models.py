8fd91c2644d46efdb7b05ba12ff7dd4b50196c23
Skiff <skiff.udev@gmail.com>
2019-10-25 16:40:09 +0300

    init

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.shortcuts import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+    date_pub = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
+
+from .models import Post, Tag
+from .utils import ObjectDetailMixin
+from .forms import TagForm, PostForm
+
+
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_create_form.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+
+        return render(request, 'blog/post_create_form.html', context={'form': bound_form})
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
